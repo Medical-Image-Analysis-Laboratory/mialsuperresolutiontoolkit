d9e5da47d30b5b1b87c5409018b2cb4c0d7b1679
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2017-07-03 16:17:13 +0200

    Update mialsrtkCorrectBiasFieldWithMotionApplied.cxx

--- src/IntensityStandardization/mialsrtkCorrectBiasFieldWithMotionApplied.cxx
+++ src/IntensityStandardization/mialsrtkCorrectBiasFieldWithMotionApplied.cxx
@@ -6,6 +6,10 @@ Date: $Date: 2016-28-06 15:00:00 +0100 (Tuesday, 28 June 2016) $
 Version: $Revision: 1 $
 Author: $Sebastien Tourbier$
 
+Copyright (c) 2017 Medical Image Analysis Laboratory (MIAL), Lausanne
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+     
 ==========================================================================*/
 
 #include <tclap/CmdLine.h>
