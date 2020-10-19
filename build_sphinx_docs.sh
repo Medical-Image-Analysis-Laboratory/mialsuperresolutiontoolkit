543bb1da534063a789cc8ad9b5ffa48a8fd4e1ea
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-10-19 16:08:01 +0200

    DOC: Improve way to get directory of script for doc on macosx

--- build_sphinx_docs.sh
+++ build_sphinx_docs.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         # Linux
         DIR="$(dirname $(readlink -f "$0"))"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
-        DIR="$(dirname "$0")"
+        DIR=$(dirname $(realpath $0))
 fi
 
 echo "Building documentation in $DIR/documentation/_build/html"
