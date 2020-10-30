89a3a7fc4bc266d05f4993f0da54e90044e7cd92
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-10-30 13:53:19 +0100

    MAINT: fix code style issue in script reported by codacy

--- build_jupyter.sh
+++ build_jupyter.sh
@@ -1,25 +1,25 @@
 #! /bin/sh
-CMP_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-echo $CMP_BUILD_DATE
+CMP_BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+echo "$CMP_BUILD_DATE"
 
-VERSION=v$(python get_version.py)
-echo $VERSION
+VERSION="v$(python get_version.py)"
+echo "$VERSION"
 
-VCS_REF=$(git rev-parse --verify HEAD)
-echo $VCS_REF
+VCS_REF="$(git rev-parse --verify HEAD)"
+echo "$VCS_REF"
 
 MAIN_DOCKER="sebastientourbier/mialsuperresolutiontoolkit-ubuntu14.04:${VERSION}"
-echo $MAIN_DOCKER
+echo "$MAIN_DOCKER"
 
-docker build --rm --build-arg BUILD_DATE=$CMP_BUILD_DATE \
-				  --build-arg VCS_REF=$VCS_REF \
-				  --build-arg VERSION=$VERSION \
+docker build --rm --build-arg BUILD_DATE="$CMP_BUILD_DATE" \
+				  --build-arg VCS_REF="$VCS_REF" \
+				  --build-arg VERSION="$VERSION" \
 				  -t "${MAIN_DOCKER}" . \
 
 JUPYTER_DOCKER="sebastientourbier/mialsuperresolutiontoolkit-jupyter:${VERSION}"
 
-docker build --rm --build-arg BUILD_DATE=$CMP_BUILD_DATE \
-                             --build-arg VERSION=$VERSION \
-                             --build-arg VCS_REF=$VCS_REF \
-                             --build-arg MAIN_DOCKER=$MAIN_DOCKER \
+docker build --rm --build-arg BUILD_DATE="$CMP_BUILD_DATE" \
+                             --build-arg VERSION="$VERSION" \
+                             --build-arg VCS_REF="$VCS_REF" \
+                             --build-arg MAIN_DOCKER="$MAIN_DOCKER" \
                              -t "${JUPYTER_DOCKER}" ./docker/jupyter
