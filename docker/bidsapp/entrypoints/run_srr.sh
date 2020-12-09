4e4c5052509263753cfb08012f7d4b347866c77a
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-09 17:59:37 +0100

    DOCKER: refactoring of entrypoint scripts

---
+++ docker/bidsapp/entrypoints/run_srr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "User: $(id -un $USER)" && echo "Group: $(id -gn $USER)" && \
+. activate "${MY_CONDA_PY3ENV}" && \
+xvfb-run -a python /opt/mialsuperresolutiontoolkit/docker/bidsapp/run.py $@
