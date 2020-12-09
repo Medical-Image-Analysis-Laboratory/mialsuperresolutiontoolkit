47a1cbd1701885d3c91ba9352c5ac361d5989a0e
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-09 18:08:46 +0100

    MAINT: correct code styles raised by codacy

--- docker/bidsapp/entrypoints/run_srr.sh
+++ docker/bidsapp/entrypoints/run_srr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "User: $(id -un $USER)" && echo "Group: $(id -gn $USER)" && \
+echo "User: $(id -un "$USER")" && echo "Group: $(id -gn "$USER")" && \
 . activate "${MY_CONDA_PY3ENV}" && \
-xvfb-run -a python /opt/mialsuperresolutiontoolkit/docker/bidsapp/run.py $@
+xvfb-run -a python /opt/mialsuperresolutiontoolkit/docker/bidsapp/run.py "$@"
