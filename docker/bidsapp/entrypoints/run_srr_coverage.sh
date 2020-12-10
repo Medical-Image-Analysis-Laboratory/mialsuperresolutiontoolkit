ec54674e034fb0513fb43676ebda52e4461f605a
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-10 09:27:01 +0100

    FIX: typo in run_srr_coverage.sh

--- docker/bidsapp/entrypoints/run_srr_coverage.sh
+++ docker/bidsapp/entrypoints/run_srr_coverage.sh
@@ -3,7 +3,7 @@ echo "User: $(id -un "$USER")" && echo "Group: $(id -gn "$USER")" && \
 export && \
 echo "SHELL: $SHELL" && \
 echo "PATH: $PATH" && \
-. activate "${$MY_CONDA_PY3ENV}" && \
+. activate "${MY_CONDA_PY3ENV}" && \
 xvfb-run -a coverage run --source=pymialsrtk \
 /opt/mialsuperresolutiontoolkit/docker/bidsapp/run.py "$@" \
 |& tee /bids_dir/code/log.txt && \
