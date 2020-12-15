e965c5c02455993b26658f1d992cfd41437f3efa
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-15 12:37:40 +0100

    SETUP: updated scripts appropriately and corrected invalid PEP indent

--- setup.py
+++ setup.py
@@ -74,53 +74,54 @@ class VerifyVersionCommand(install):
 
 def main():
     """Main function of the PyMIALSRTK ``setup.py``"""
-    setuptools.setup(name='pymialsrtk',
-          version=__version__,
-          description='PyMIALSRTK: Nipype pipelines for the MIAL Super Resolution Toolkit ',
-          long_description="""PyMIALSRTK interfaces with MIALSRTK C++ tools and implements
+    setuptools.setup(
+            name='pymialsrtk',
+            version=__version__,
+            description='PyMIALSRTK: Nipype pipelines for the MIAL Super Resolution Toolkit ',
+            long_description="""PyMIALSRTK interfaces with MIALSRTK C++ tools and implements
                               a full processing pipeline using the NiPype dataflow library,
                               from motion-corrupted anisotropic multi-slice MRI scans
                               to a motion-free isotropic high-resolution image. """,
-          author='Sebastien Tourbier',
-          author_email='sebastien.tourbier@alumni.epfl.ch',
-          url='https://github.com/Medical-Image-Analysis-Laboratory/mialsuperresolutiontoolkit',
-          entry_points={
+            author='Sebastien Tourbier',
+            author_email='sebastien.tourbier@alumni.epfl.ch',
+            url='https://github.com/Medical-Image-Analysis-Laboratory/mialsuperresolutiontoolkit',
+            entry_points={
                  "console_scripts": [
-                         'mialsuperresolutiontoolkit_bidsapp_docker = pymialsrtk.cli.mialsuperresolutiontoolkit_bidsapp_docker:main',
-                         'mialsuperresolutiontoolkit_bidsapp_singularity = pymialsrtk.cli.mialsuperresolutiontoolkit_bidsapp_singularity:main'
+                     'mialsuperresolutiontoolkit_docker = pymialsrtk.cli.mialsuperresolutiontoolkit_docker:main',
+                     'mialsuperresolutiontoolkit_singularity = pymialsrtk.cli.mialsuperresolutiontoolkit_singularity:main'
                  ]
-          },
-          license='BSD-3-Clause',
-          classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved',
-            'Programming Language :: C++',
-            'Programming Language :: Python',
-            'Topic :: Software Development',
-            'Topic :: Scientific/Engineering :: Image Processing',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: POSIX',
-            'Operating System :: Unix',
-            'Operating System :: MacOS',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-          ],
-          maintainer='Medical Image Analysis Laboratory, University Hospital of Lausanne and the MIALSRTK developers',
-          maintainer_email='sebastien.tourbier@alumni.epfl.ch',
-          # package_dir={"": "."},  # tell distutils packages are under src
-          packages=packages,
-          include_package_data=True,
-          package_data=package_data,
-          # exclude_package_data={"": ["README.txt"]},
-          install_requires=install_requires,
-          dependency_links=dependency_links,
-          python_requires='>=3.6',
-          cmdclass={
-            'verify': VerifyVersionCommand,
-          }
-          )
+            },
+            license='BSD-3-Clause',
+            classifiers=[
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Science/Research',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved',
+                'Programming Language :: C++',
+                'Programming Language :: Python',
+                'Topic :: Software Development',
+                'Topic :: Scientific/Engineering :: Image Processing',
+                'Operating System :: Microsoft :: Windows',
+                'Operating System :: POSIX',
+                'Operating System :: Unix',
+                'Operating System :: MacOS',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+            ],
+            maintainer='Medical Image Analysis Laboratory, University Hospital of Lausanne and the MIALSRTK developers',
+            maintainer_email='sebastien.tourbier@alumni.epfl.ch',
+            # package_dir={"": "."},  # tell distutils packages are under src
+            packages=packages,
+            include_package_data=True,
+            package_data=package_data,
+            # exclude_package_data={"": ["README.txt"]},
+            install_requires=install_requires,
+            dependency_links=dependency_links,
+            python_requires='>=3.6',
+            cmdclass={
+                'verify': VerifyVersionCommand,
+            }
+            )
 
 
 if __name__ == "__main__":
