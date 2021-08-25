585f3a240a7ef24564555eca0e8214b8ccffef5a
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2021-08-25 18:57:29 +0200

    UPD: Update release date in `info.py`

--- pymialsrtk/info.py
+++ pymialsrtk/info.py
@@ -5,7 +5,7 @@ _version_minor = 0
 _version_micro = 2
 _version_extra = ''
 
-__release_date__ = '25.08.2021'
+__release_date__ = '26.08.2021'
 
 __minor_version__ = "%s.%s" % (_version_major,
                                _version_minor)
